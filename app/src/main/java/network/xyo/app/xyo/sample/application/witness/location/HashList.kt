5200e1949376297e840acb5b0ed6cacf65c4f468
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-11-15 17:18:26 -0500

    launch chrome when hashes are clicked

--- app/src/main/java/network/xyo/app/xyo/sample/application/witness/location/HashList.kt
+++ app/src/main/java/network/xyo/app/xyo/sample/application/witness/location/HashList.kt
@@ -1,5 +1,8 @@
 package network.xyo.app.xyo.sample.application.witness.location
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +16,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -39,10 +43,17 @@ fun HashList(hashesViewModel: HashesViewModel) {
 
 @Composable
 fun HashItem(hash: String) {
+    val context = LocalContext.current // Access the current context
+
     Box(
         Modifier.border(1.dp, Color.Gray)
     ) {
-        Text(text = hash, modifier = Modifier.padding(10.dp))
+        Text(text = hash, modifier = Modifier
+            .padding(10.dp).clickable {
+                // Open the link in the browser
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://beta.explore.xyo.network/$hash"))
+                context.startActivity(intent)
+            })
     }
 
 }
